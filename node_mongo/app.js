37301f32e9d2a816321f8e98572cd778b37da268
AnnatarHe <j692356371@126.com>
2015-06-25 09:32:14 +0800

    add models

--- node_mongo/app.js
+++ node_mongo/app.js
@@ -1,8 +1,11 @@
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
+var Movie = require('./models/movie')
 var port = process.env.PORT || 3000
 var app = express()
+
+mongoose.connect('mongoose://localhost/movie')
 
 app.set('views','./views/pages')
 app.set('view engine','jade')
@@ -13,37 +16,37 @@ app.listen(port)
 console.log('port ' + port + ' was listened')
 //there is index page
 app.get('/',function(req,res){
-    res.render('index.jade',{
-        'title':'hello hele',
-        'movies':[
-        {
-            'title':'hadhf',
-            '_id':'1',
-            'poster':'fd'
-        },
-        {
-            'title':'hadhf',
-            '_id':'2',
-            'poster':'fd'
+    Movie.fetch(function(err,movies){
+        if (error) {
+            console.log(err)
         }
-        ]
+        res.render('index.jade',{
+            title:'Index page',
+            movies:movies
+        })
     })
+
+    
 })
 //there is list page
 app.get('/admin/list',function(req,res){
     res.render('list.jade',{
-        'title':'there is list page'
+        title:'there is list page'
     })
 })
 //detail page
 app.get('/movies/:id',function(req,res){
-    res.render('detail.jade',{
-        'title':'there is detail page'
+    var id = req.params.id
+
+    Movie.findOneById(id,function(err,movie){
+        res.render('detail.jade',{
+            title:'there is detail page'
+        })
     })
 })
 //admin page
 app.get('/admin/movie',function(req,res){
     res.render('admin.jade',{
-        'title':'there is admin page'
+        title:'there is admin page'
     })
 })
