17b24d3fdaf518a6bc74713a0d42000ccc9e86bb
AnnatarHe <j692356371@126.com>
2015-06-25 10:21:35 +0800

    run
    run

--- node_mongo/app.js
+++ node_mongo/app.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
 var bodyParser = require('body-parser')
 var Movie = require('./models/movie')
 var port = process.env.PORT || 3000
 var app = express()
 
-mongoose.connect('mongoose://localhost/movie')
+var db = mongoose.connect('mongodb://localhost:27017/movie')
 
 app.set('views','./views/pages')
 app.set('view engine','jade')
@@ -17,7 +19,7 @@ console.log('port ' + port + ' was listened')
 //there is index page
 app.get('/',function(req,res){
     Movie.fetch(function(err,movies){
-        if (error) {
+        if (err) {
             console.log(err)
         }
         res.render('index.jade',{
@@ -30,23 +32,96 @@ app.get('/',function(req,res){
 })
 //there is list page
 app.get('/admin/list',function(req,res){
-    res.render('list.jade',{
-        title:'there is list page'
+    Movie.fetch(function(err,movies){
+        if (error) {
+            console.log(err)
+        }
+        res.render('list.jade',{
+            title:'list page',
+            movies:movies
+        })
     })
 })
 //detail page
 app.get('/movies/:id',function(req,res){
     var id = req.params.id
 
-    Movie.findOneById(id,function(err,movie){
+    Movie.findById(id,function(err,movie){
         res.render('detail.jade',{
-            title:'there is detail page'
+            title:'Movie' + movie.title,
+            movie:movie
         })
     })
 })
 //admin page
 app.get('/admin/movie',function(req,res){
     res.render('admin.jade',{
-        title:'there is admin page'
+        title:'there is admin page',
+        movie:{
+            title:'',
+            director:'',
+            country:'',
+            year:'',
+            poster:'',
+            flash:'',
+            sumary:'',
+            language:''
+        }
     })
+})
+//admin update
+app.get('/admin/movie/update/:id',function(req,res){
+    var id = req.params.id
+    if (id) {
+        Movie.findById(id,function(err,movie){
+            if (err) {
+                colsole.log(err)
+            }
+            res.render('admin',{
+                title:'Update Page',
+                movie:movie
+            })
+        })
+    };
+})
+
+
+//admin post movie
+app.get('/admin/movie/new',function(req,res){
+    var id = req.body.movie._id
+    var movieObj = req.body.movie
+    var _movie
+
+    if (id !== 'undefined') {
+        Movie.findById(id,function(err,movie){
+            if (err) {
+                console.log(err)
+            }
+            _movie = _.extend(movie,movieObj)
+            _movie.save(function(err,movie){
+                if (err) {
+                    console.log(err)
+                }
+
+                res.redirect('/movie/' + movie._id)
+            })
+        })
+    }else{
+        _movie = new Movie({
+            director:movieObj.director,
+            title:movieObj.title,
+            country:movieObj.country,
+            language:movieObj.language,
+            year:movieObj.year,
+            poster:movieObj.poster,
+            sumary:movieObj.sumary,
+            flash:movieObj.flash
+        })
+        _movie.save(function(err,movie){
+            if (err) {
+                console.log(err)
+            }
+            res.redirect('/movie/' + movie._id)
+        })
+    }
 })
--- node_mongo/schemas/movies.js
+++ node_mongo/schemas/movies.js
@@ -29,12 +29,10 @@ MoviesSchema.pre('save',function(next){
 })
 MoviesSchema.statics = {
 	fetch:function(cb){
-		return this.find({}).sort('meta.updateAt')
-		exec(cb)
+		return this.find({}).sort('meta.updateAt').exec(cb)
 	},
-	findBuId:function(id,cb){
-		return this.findOne({_id:id})
-		exec(cb)
+	findById:function(id,cb){
+		return this.findOne({_id:id}).exec(cb)
 	}
 }
 module.exports = MoviesSchema
