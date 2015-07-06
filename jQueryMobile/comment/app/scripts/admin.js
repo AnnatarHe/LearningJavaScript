44252d776fe4eb7925692c433e3149a94736d501
AnnatarHe <hele@iamhele.com>
2015-07-06 21:56:47 +0800

    annotion was added

--- jQueryMobile/comment/app/scripts/admin.js
+++ jQueryMobile/comment/app/scripts/admin.js
@@ -1,26 +1,28 @@
 // jshint devel:true
 $(function(){
+    /**
+     * 单击获取数据
+     */
+    console.log('copyright iamhele.com');
     $('#getInfo').click(function(){
-        console.log('the click is working');
         $.ajax({
             url:'../uploads/info.js',
             type:'GET',
             dataType:'text',
             success:function(data){
+            //性能问题，使用了字符串的切割函数
+            //之后补上。
                var ArrayData = data.split('}');
-               console.log(ArrayData);
                //所有都做完了，现在是往里面添加数据。
-               console.log(ArrayData.length);
                for(var i=0;i<ArrayData.length;i++)
                 {
                    var json = JSON.parse(ArrayData[i]+'}');
-                   console.log(json);
                    var html = '<tr><td>'+json.number+'</td><td>'+json.name+'</td><td>'+json.contact+'</td><td>'+json.content+'</td></tr>';
                    $('.table').append(html);
                }
             },
-            error:function(data,info){
-                console.log('error'+info);
+            error:function(){
+                console.log('出现了错误，请联系 hele@iamhele.com 进行解决');
             }
         });
         /*ajax stop*/
