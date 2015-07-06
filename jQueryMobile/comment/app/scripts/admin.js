437eaaa887a5daa99322aa9cf6c784789f8b7662
AnnatarHe <hele@iamhele.com>
2015-07-06 10:44:24 +0800

    repeat infomation

--- jQueryMobile/comment/app/scripts/admin.js
+++ jQueryMobile/comment/app/scripts/admin.js
@@ -7,11 +7,14 @@ $(function(){
             type:'GET',
             dataType:'text',
             success:function(data){
-                console.log(data);
-                console.log(data[0].number);
-                var regx = '/^/{*/}/';
-                var rs = regx.match(data);
-                console.log(rs);
+               var ArrayData = data.split('}');
+               console.log(ArrayData);
+               //所有都做完了，现在是往里面添加数据。
+               for i in ArrayData{
+                   var json = JSON.parse(ArrayData[i]+'}');
+                   var html = '<tr><td>'+json.number+'</td><td>'+json.name+'</td><td>'+json.contact+'</td><td>'+json.content+'</td></tr>';
+                   $('.table').append(html);
+                   }
             },
             error:function(data,info){
                 console.log('error'+info);
