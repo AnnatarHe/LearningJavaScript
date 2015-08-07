b3cf34f71087a99c77b9efd359d05a891afd2fde
AnnatarHe <j692356371@126.com>
2015-08-07 21:47:50 +0800

    add proxy class
    proxy class. the Microsoft Edge is the fullly suport ES6 than Chrome 44

--- es6/src/hello.js
+++ es6/src/hello.js
@@ -1,8 +1,30 @@
-let a = 1;
-class hello {
-	hi(){
-		console.log('hello world');
+(function(){
+	let a = 1;
+	const b = 3;
+	class hello {
+		hi(){
+			console.log('hello world');
+		}
 	}
+
+
+// proxy
+var target = {};
+var handler = {
+  get: function (receiver, name) {
+    return `Hello, ${name}!`;
+  }
+};
+
+var p = new Proxy(target, handler);
+console.log(p.world === "Hello, world!");
+
+// default values in function
+
+function foo(x = 'foo') {
+	console.log(`hello ${x}`);
 }
-var foo = new hello;
-foo.hi();
+foo()
+
+
+}());
