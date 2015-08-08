7e7c81466c9ff97af3fa2e8c488c17ad83dc0832
AnnatarHe <j692356371@126.com>
2015-08-08 10:10:07 +0800

    add set rest and etc...

--- es6/src/hello.js
+++ es6/src/hello.js
@@ -24,7 +24,39 @@ console.log(p.world === "Hello, world!");
 function foo(x = 'foo') {
 	console.log(`hello ${x}`);
 }
-foo()
+foo();
+
+// rest
+function bar(...params) {
+	let sum = 0;
+	for(let i = 0 of params){
+		sum += i;
+	}
+	return sum;
+}
+
+bar(3,6,9,5,4,2);
+
+
+let sum = (num1, num2) => num1 + num2;
+
+var getJson = id => ({ id: id, name: "Temp" });
+
+
+function factorial(n, total) {
+  if (n === 1) return total;
+  return factorial(n - 1, n * total);
+}
+
+factorial(5, 1) // 120
+
+
+
+let s = new Set();
+
+[2,3,5,4,5,2,2].map(x => s.add(x));
+
+for (i of s) {console.log(i)}
 
 
 }());
