4afe7c969cef8696ec07a7853baa7bcbe54a505d
AnnatarHe <hele@iamhele.com>
2015-08-25 23:41:12 +0800

    unstanding react

--- iamheleAngular/app/js/app.js
+++ iamheleAngular/app/js/app.js
@@ -1,3 +1,10 @@
 var article = angular.module('article',['ngAnimate', 'ngRoute']);
 article.controller('hi',['$scope',function($scope) {
-}]);
+        }])
+        .controller('ArticleListController', ['$scope', '$http', function($scope, $http) {
+          $scope.articles = $http({
+            url: '/fake/articles.json',
+            method: 'GET'
+          });
+          console.log($scope.articles);
+        }]);
