7c006b6af678d6b5e5b7b04e447a33ff6eecf89d
AnnatarHe <hele@iamhele.com>
2015-08-26 22:31:52 +0800

    ng Route is runing

--- iamhele-Front-end/app/js/main.js
+++ iamhele-Front-end/app/js/main.js
@@ -1,1 +1,16 @@
 var blog = angular.module('blog', ['ngAnimate', 'ngRoute']);
+blog.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/articles', {
+    templateUrl: '/dist/scripts/partials/articles.html',
+    controller: 'ArticlesController'
+  })
+  .otherwise({
+    redirectTo: '/articles'
+  })
+}])
+.controller('listsController' ,['$scope', '$http', function($scope, $http) {
+  let a = 1;
+}])
+.controller('ArticlesController', ['$scope',function($scope) {
+
+}]);
