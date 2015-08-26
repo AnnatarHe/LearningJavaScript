8b656c7b60200a7ee770f2e6aee819a0b624f40e
AnnatarHe <hele@iamhele.com>
2015-08-26 22:52:09 +0800

    3 partials was be build

--- iamhele-Front-end/app/js/main.js
+++ iamhele-Front-end/app/js/main.js
@@ -4,13 +4,27 @@ blog.config(['$routeProvider', function($routeProvider) {
     templateUrl: '/dist/scripts/partials/articles.html',
     controller: 'ArticlesController'
   })
+  .when('/articles/:articleId', {
+    templateUrl: '/dist/scripts/partials/detail.html',
+    controller: 'DetailController'
+  })
+  .when('/about', {
+    templateUrl: '/dist/scripts/partials/about.html',
+    controller: 'AboutController'
+  })
   .otherwise({
     redirectTo: '/articles'
-  })
+  });
 }])
 .controller('listsController' ,['$scope', '$http', function($scope, $http) {
   let a = 1;
 }])
 .controller('ArticlesController', ['$scope',function($scope) {
 
-}]);
+}])
+.controller('DetailController', ['$scope', '$http', '$routeParams', function() {
+  console.log($routeParams.articleId);
+}])
+.controller('AboutController', ['$scope', function() {
+  
+}])
